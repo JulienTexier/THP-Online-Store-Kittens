d61dfead1b71331f769382f9957bac644e289ef1
PaulMouchel <paulmouchel@live.fr>
2019-09-02 20:27:12 +0200

    View show cart first version

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,4 +2,12 @@ class Cart < ApplicationRecord
 	belongs_to :user
 	has_many :join_table_carts_items
 	has_many :items, through: :join_table_carts_items
+
+	def subtotal
+		price = 0.0
+		self.items.each do |item|
+			price += item.price
+		end
+		return price
+	end
 end
