ad7cbaea081f87c697268369bf1fad59affdabfd
JulienTexier <juu.texier@gmail.com>
2019-09-03 15:05:15 +0200

    adding cart feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-
 	root 'items#index'
-
+  resources :join_items_orders
+  resources :orders
 	resources :join_table_carts_items
-  resources :carts, except: [:index]
-
+  resources :carts
   resources :items
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'registrations'
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
