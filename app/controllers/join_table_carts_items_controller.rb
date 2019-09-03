053bbae9765ebf91e3235af97b88eff9ce1d358b
PaulMouchel <paulmouchel@live.fr>
2019-09-03 12:12:06 +0200

    Delete item in cart

--- app/controllers/join_table_carts_items_controller.rb
+++ app/controllers/join_table_carts_items_controller.rb
@@ -1,2 +1,18 @@
 class JoinTableCartsItemsController < ApplicationController
+	before_action :set_join_cart_item, only: [:destroy]
+
+	def destroy
+		@join_cart_item.destroy
+    respond_to do |format|
+      format.html { redirect_to cart_path(current_user.cart), notice: 'item was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+	end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_join_cart_item
+    @join_cart_item = JoinTableCartsItem.find(params[:id])
+  end
+
 end
