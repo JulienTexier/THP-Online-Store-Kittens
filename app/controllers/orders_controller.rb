54e2e17809a16f390ed40b130883acfa12a056f9
JulienTexier <juu.texier@gmail.com>
2019-09-04 11:44:27 +0200

    adding stripe

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -24,6 +24,20 @@ class OrdersController < ApplicationController
   # POST /orders
   # POST /orders.json
   def create
+    @amount = (current_user.cart.subtotal * 100).to_i
+  
+    customer = Stripe::Customer.create({
+      email: params[:stripeEmail],
+      source: params[:stripeToken],
+    })
+  
+    charge = Stripe::Charge.create({
+      customer: customer.id,
+      amount: @amount,
+      description: 'Rails Stripe customer',
+      currency: 'eur',
+    })
+
     @order = Order.new(user: current_user, items: current_user.cart.items)
     current_user.cart.items.clear
     respond_to do |format|
@@ -38,6 +52,10 @@ class OrdersController < ApplicationController
         format.json { render json: @order.errors, status: :unprocessable_entity }
       end
     end
+
+    rescue Stripe::CardError => e
+      flash[:error] = e.message
+      redirect_to new_charge_path
   end
 
   # PATCH/PUT /orders/1
